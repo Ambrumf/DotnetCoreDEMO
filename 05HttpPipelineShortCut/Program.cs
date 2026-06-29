3393450e11bbac66c5cca8dc3341309e1a1e20bd
Ambrumf <421653227@qq.com>
2026-06-29 15:30:44 +0800

    add HttpPipelineShortCut DEMO

---
+++ 05HttpPipelineShortCut/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Use(async(context, next) =>
+{
+    context.Response.Headers.Add("X-tag", "Demo");
+    await next();
+});
+
+
+app.Use(async (context, next) =>
+{
+    var request = context.Request;
+    var response = context.Response;
+    if (request.Query["flag"] == "1")
+    {
+        await response.WriteAsync("Flag = 1");
+    }
+    else await next();
+});
+
+app.Run();
